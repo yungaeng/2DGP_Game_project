3d669fc2c622a7bf1300ed95256d729c28b76348
ku yun soeng <kys1091201@naver.com>
2022-10-08 13:37:08 +0900

    commit 2
    배경 소년 클래스지정

--- boy.py
+++ boy.py
@@ -1,49 +1,80 @@
 from pico2d import *
 
+
+class Background:
+    def __init__(self):
+        self.image = load_image('background.png')
+
+    def draw(self):
+        self.image.draw(400, 300)
+
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 400, 90
+        self.frame = 0
+        self.base_frame = 0
+        self. dir = 0
+        self.image = load_image('anisheet.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 10
+        self.base_frame = (self.base_frame + 1) % 2
+        self.x += self.dir * 1
+
+        if self.x > 800:
+            self.x = 800
+            self.dir = -1
+        elif self.x < 0:
+            self.x = 0
+            self.dir = 1
+
+    def draw(self):
+        if self.dir == 1:
+            self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+        elif self.dir == -1:
+            self.image.clip_draw(self.frame * 100, 200, 100, 100, self.x, self.y)
+        else:
+            self.image.clip_draw(self.base_frame * 100, 700, 100, 100, self.x, self.y)
+
+
 def handle_events():
-    global running, x, dir
+    global running
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_ESCAPE:
-                running = False
-            elif event.key == SDLK_RIGHT:
-                dir += 1
-            elif event.key == SDLK_LEFT:
-                dir -= 1
-            elif event.key == SDLK_ESCAPE:
-                running = False
+            match event.key:
+                case pico2d.SDLK_ESCAPE:
+                    running = False
+                case pico2d.SDLK_LEFT:
+                    boy.dir -= 1
+                case pico2d.SDLK_RIGHT:
+                    boy.dir += 1
         elif event.type == SDL_KEYUP:
-            if event.key == SDLK_RIGHT:
-                dir -= 1
-            elif event.key == SDLK_LEFT:
-                dir += 1
+            match event.key:
+                case pico2d.SDLK_LEFT:
+                    boy.dir += 1
+                case pico2d.SDLK_RIGHT:
+                    boy.dir -= 1
+
 
 open_canvas()
 
-background = load_image('background.png')
-character = load_image('anisheet.png')
+boy = Boy()
+background = Background()
 
 running = True
-x = 800 // 2
-frame = 0
-dir = 0
 
 while running:
-    clear_canvas()
-    background.draw(400, 300)
-    if dir == 1:
-        character.clip_draw(frame * 100, 0, 100, 100, x, 100)
-    elif dir == -1:
-        character.clip_draw(frame * 100, 200, 100, 100, x, 100)
-    else :
-        character.clip_draw(frame * 100, 700, 100, 100, x, 100)
-    update_canvas()
     handle_events()
-    frame = (frame + 1) % 3
-    x += dir * 5
-    delay(0.01)
 
-close_canvas()
+    boy.update()
+
+    clear_canvas()
+    background.draw()
+    boy.draw()
+    update_canvas()
+
+    delay(0.001)
